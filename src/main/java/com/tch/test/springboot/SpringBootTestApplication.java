e78162858ec15cc5e0c8f40662804146ae4202e9
tianch <tianch@inmindglobal.com>
2017-05-19 12:23:49 +0800

    调整之后，可以运行起来

--- src/main/java/com/tch/test/springboot/SpringBootTestApplication.java
+++ src/main/java/com/tch/test/springboot/SpringBootTestApplication.java
@@ -1,4 +1,4 @@
-package com.tch.test.springboottest;
+package com.tch.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
